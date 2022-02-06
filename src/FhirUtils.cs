bb29d65db98a3462b6268504593a8720b5dc31ef
bnaoe <bp.naoe@gmail.com>
2022-02-05 20:25:44 -0600

    Setup OAuth-uris, get authorize, token from FHIR server

---
+++ src/FhirUtils.cs
@@ -0,0 +1,71 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
+
+namespace smart_local
+{
+    /// <summary>
+    /// FHIR Utility Functions
+    /// </summary>
+    public static class FhirUtils
+    {
+        public static bool TryGetSmartUrls(
+            FhirClient fhirClient,
+            out string authorizeUrl,
+            out string tokenUrl
+        )
+        {
+            authorizeUrl = string.Empty;
+            tokenUrl = string.Empty;
+            CapabilityStatement capabilities = (CapabilityStatement)fhirClient.Get("metadata");
+
+            foreach (CapabilityStatement.RestComponent restComponent in capabilities.Rest)
+            {
+                if (restComponent.Security ==null)
+                {
+                    continue;
+                }
+
+                foreach (Extension securityExt in restComponent.Security.Extension)
+                {
+                    if (securityExt.Url != "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris")
+                    {
+                        continue;
+                    }
+
+                    if (securityExt.Extension ==null || securityExt.Extension.Count==0)
+                    {
+                        continue;
+                    } 
+
+                    foreach(Extension smartExt in securityExt.Extension)
+                    {
+                        switch (smartExt.Url)
+                        {
+                            case "authorize":
+                                authorizeUrl = ((FhirUri)smartExt.Value).Value.ToString();
+                                //smartExt.GetExtensionValue<FhirUri>("authorize").Value.ToString();
+                                break;
+
+                            case "token":
+                                tokenUrl = ((FhirUri)smartExt.Value).Value.ToString();
+                                //smartExt.GetExtensionValue<FhirUri>("token").ToString();
+                                break;
+                        }
+                    }
+                }
+            }
+            if (string.IsNullOrEmpty(authorizeUrl)||string.IsNullOrEmpty(tokenUrl))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+
---
+++ src/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace smart_local
+{
+    /// <summary>
+    /// Main Program
+    /// </summary>
+    public static class Program
+    {
+        private const string _defaultFhirServerUrl 
+        ="https://launch.smarthealthit.org/v/r4/sim/eyJoIjoiMSIsImUiOiJlNDQzYWM1OC04ZWNlLTQzODUtOGQ1NS03NzVjMWI4ZjNhMzcifQ/fhir/";
+        
+        /// <summary>
+        /// Program to access a SMART FHIR Server with a local webserver for redirection
+        /// </summary>
+        /// <param name="fhirServerUrl">FHIR R4 endpoint URL</param>
+        /// <returns></returns>
+        static int Main (
+            string fhirServerUrl)
+        {
+            if (string.IsNullOrEmpty(fhirServerUrl))
+            {
+                fhirServerUrl = _defaultFhirServerUrl;
+            }
+            System.Console.WriteLine($" FHIR Server : {fhirServerUrl} ");
+
+            Hl7.Fhir.Rest.FhirClient fhirClient = new Hl7.Fhir.Rest.FhirClient(fhirServerUrl);
+            
+            if(!FhirUtils.TryGetSmartUrls(fhirClient, out string authorizeUrl, out string tokenUrl))
+            {
+                System.Console.WriteLine("Failed to get SMART URLs");
+                return -1;
+            }
+            
+            System.Console.WriteLine($"Authorize URL : {authorizeUrl}");
+            System.Console.WriteLine($"    Token URL : {tokenUrl}");
+
+            return 0;
+        }
+    }
+}
+
+
